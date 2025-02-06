a01332248e00e06513dadea58d774c3c1f3f59c4
izucchii <johnpaulbarlongo58@gmail.com>
2025-02-06 19:34:13 +0800

    Circular Linked List

---
+++ LinkedList/CircularLinkedList/CircularLinkedList.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+using namespace std;
+
+struct Node {
+    int data;
+    Node* next;
+};
+
+class CircularLinkedList {
+private:
+    Node* head;
+
+public:
+    CircularLinkedList() {
+        head = nullptr;
+    }
+
+    void insert(int value) {
+        Node* newNode = new Node();
+        newNode->data = value;
+        if (head == nullptr) {
+            head = newNode;
+            newNode->next = head;
+        } else {
+            Node* temp = head;
+            while (temp->next != head) {
+                temp = temp->next;
+            }
+            temp->next = newNode;
+            newNode->next = head;
+        }
+    }
+
+    void display() {
+        if (head == nullptr) {
+            cout << "List is empty!" << endl;
+            return;
+        }
+        Node* temp = head;
+        do {
+            cout << temp->data << " -> ";
+            temp = temp->next;
+        } while (temp != head);
+        cout << "(head)" << endl;
+    }
+};
+
+int main() {
+    CircularLinkedList cll;
+    cll.insert(10);
+    cll.insert(20);
+    cll.insert(30);
+    cll.insert(40);
+    
+    cout << "Circular Linked List: ";
+    cll.display();
+
+    return 0;
+}
---
+++ LinkedList/CircularLinkedList/README.md
@@ -0,0 +1,14 @@
+# Circular Linked List in C++
+
+## 📌 Overview
+A **Circular Linked List** is a type of linked list where the last node points back to the first node instead of `nullptr`, forming a circular structure. This implementation supports:
+- Insertion at the end
+- Displaying the list
+
+
+
+## 🚀 How to Compile & Run
+### **1. Compile the Program**
+Use the following command to compile:
+```sh
+g++ -o cll CircularLinkedList.cpp
